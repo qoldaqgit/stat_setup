409380f8b4e240b5fac05bf0b8d4d4de22f6e79a
qoldaqgit <152833825+qoldaqgit@users.noreply.github.com>
2025-10-14 08:56:31 -0400

    Update install.sh

--- ci4/install.sh
+++ ci4/install.sh
@@ -1,6 +1,8 @@
 #sudo curl -sSL https://raw.githubusercontent.com/qoldaqgit/stat_setup/refs/heads/main/ci4/install.sh
 sudo apt update
-sudo apt install php phpunit composer -y
+sudo apt install apache2  php zip unzip php-zip php-curl libapache2-mod-php php8.1-mbstring php-intl php-mysql gh nano ssmtp git -y 
+sudo apt install php-cli php-json php-mbstring php-xml php-pcov php-xdebug -y
+sudo apt install composer -y
 
 #download CI4 easy_scripts
 wget -q https://raw.githubusercontent.com/qoldaqgit/stat_setup/refs/heads/main/ci4/create_project.sh
