605b99d72cdd0a0ac7e7b45cf51e6060e9a102e9
qoldaqgit <152833825+qoldaqgit@users.noreply.github.com>
2025-10-13 23:46:44 -0400

    Update install.sh

--- ci4/install.sh
+++ ci4/install.sh
@@ -5,4 +5,4 @@ sudo apt install php phpunit composer -y
 #download CI4 easy_scripts
 wget -q https://raw.githubusercontent.com/qoldaqgit/stat_setup/refs/heads/main/ci4/create_project.sh
 wget -q https://raw.githubusercontent.com/qoldaqgit/stat_setup/refs/heads/main/ci4/create_controller.sh
-wget -q https://raw.githubusercontent.com/qoldaqgit/stat_setup/refs/heads/main/ci4/uninstall_ci4.sh
+wget -q https://raw.githubusercontent.com/qoldaqgit/stat_setup/refs/heads/main/ci4/unistall.sh
